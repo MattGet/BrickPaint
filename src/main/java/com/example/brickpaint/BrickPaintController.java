e9f417519f71b0a2dca7ccf41bb953249f198838
Matt <87963417+MattGet@users.noreply.github.com>
2022-09-06 09:05:37 -0500

    update

--- src/main/java/com/example/brickpaint/BrickPaintController.java
+++ src/main/java/com/example/brickpaint/BrickPaintController.java
@@ -2,15 +2,14 @@ package com.example.brickpaint;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.MenuItem;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import java.io.*;
 import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.File;
 
 public class BrickPaintController {
 
@@ -18,7 +17,11 @@ public class BrickPaintController {
     private AnchorPane root;
 
     @FXML
+    private AnchorPane canvasPanel;
+
+    @FXML
     protected ImageView imageView;
+    private String ImageURL;
 
     @FXML
     private MenuItem closeButton;
@@ -36,7 +39,29 @@ public class BrickPaintController {
 
         File imageFile = new FileChooser().showOpenDialog(root.getScene().getWindow());
         Image tempImage = new Image(imageFile.toURI().toString());
+        ImageURL = imageFile.toURI().toString();
+        System.out.print(ImageURL);
         imageView.setImage(tempImage);
+    }
+
+    @FXML
+    protected void handleSaveImageAs(){
+        WritableImage imageToSave = canvasPanel.snapshot(new SnapshotParameters(), null);
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save");
+        int StartIndex = ImageURL.lastIndexOf('/');
+        String fileName = ImageURL.substring(StartIndex);
+        fileChooser.setInitialFileName(fileName);
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("images", "*.png"));
+        try{
+            File file = fileChooser.showSaveDialog(root.getScene().getWindow());
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 
     protected void OnClose(){
