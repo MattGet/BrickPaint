ea1ca3d1507867f33120ff3aca1adc849efeb2e0
Matt <87963417+MattGet@users.noreply.github.com>
2022-09-09 02:58:05 -0500

    updated package

--- src/main/java/com/example/brickpaint/brickpaintapp.java
+++ src/main/java/com/example/brickpaint/brickpaintapp.java
@@ -1,26 +1,24 @@
 package com.example.brickpaint;
 
-import com.almasb.fxgl.logging.Logger;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-import java.awt.*;
 import java.io.IOException;
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * Main application class for a javaFX paint app
  * @author matde
  */
-public class BrickPaintApp extends Application {
+public class brickpaintapp extends Application {
+    public brickpaintapp(){
+
+    }
+
     /**
      * start of javaFX program
      * @param stage
@@ -28,11 +26,11 @@ public class BrickPaintApp extends Application {
      */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BrickPaintApp.class.getResource("BrickPaint.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(brickpaintapp.class.getResource("BrickPaint.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Brick Paint");
         stage.setScene(scene);
-        Image BrickIcon = new Image(Objects.requireNonNull(BrickPaintApp.class.getResourceAsStream("BrickIcon.jpg")));
+        Image BrickIcon = new Image(Objects.requireNonNull(brickpaintapp.class.getResourceAsStream("BrickIcon.jpg")));
         stage.getIcons().add(BrickIcon);
         stage.setMaximized(true);
         BrickPaintController controller = fxmlLoader.getController();
